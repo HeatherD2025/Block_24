8ff7399bb786bf0db58792bf320aa631d5775c1e
heatherd2025 <heatherdeliso@gmail.com>
2025-04-01 14:42:22 -0400

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
 import "./App.css";
 import { puppyList } from "./data.js";
 import { useState } from "react";
+import { createRoot } from "react-dom/client";
+
+root.render;
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+
   return (
-    <>
-      <div className = "App">
+    <div className="App">
+      {puppies.map((puppy) => {
+        const featuredPup = puppies.find((pup) => pup.id === featPupId);
+        console.log(featuredPup);
         {
-          puppies.map((puppy) => {
-            return <p{puppy.id}>{puppy.name}</p>;
-          })
+          featPupId && (
+            <div>
+              <h2>{featPupId.name}</h2>
+              <ul>
+                <li>Age: {featuredPup.age}</li>
+                <li>Email: {featuredPup.email}</li>
+              </ul>
+            </div>
+          );
         }
-      </div>
-    </>
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
   );
 }
 
