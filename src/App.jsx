5964e4c96a5eb865e5bd548257f1e52860e0ebdd
heatherd2025 <heatherdeliso@gmail.com>
2025-04-01 22:13:09 -0400

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,37 @@
 import "./App.css";
 import { puppyList } from "./data.js";
 import { useState } from "react";
-import { createRoot } from "react-dom/client";
-
-root.render;
+import "./index.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
+  console.log("puppyList: ", puppyList);
 
   return (
-    <div className="App">
-      {puppies.map((puppy) => {
-        const featuredPup = puppies.find((pup) => pup.id === featPupId);
-        console.log(featuredPup);
-        {
-          featPupId && (
-            <div>
-              <h2>{featPupId.name}</h2>
-              <ul>
-                <li>Age: {featuredPup.age}</li>
-                <li>Email: {featuredPup.email}</li>
-              </ul>
-            </div>
-          );
-        }
-        return (
-          <p
-            onClick={() => {
-              setFeatPupId(puppy.id);
-            }}
-            key={puppy.id}
-          >
-            {puppy.name}
-          </p>
-        );
-      })}
+    <div
+      className="App"
+      style={{
+        backgroundImage:
+          'url("https://media.istockphoto.com/id/1154954591/photo/creative-collage-of-different-breeds-of-dogs-on-colorful-background.jpg?s=612x612&w=0&k=20&c=-_iwy8ZEGH1Zt7AfkanMJFJDuj11tw2bTRqkJgjKhfo=")',
+        backgroundRepeat: "repeat",
+        width: "100%",
+        height: "972px",
+      }}
+    >
+      <div className="boxForPuppyNames" style={{ paddingTop: "100px" }}>
+        {puppies.map((puppy) => {
+          return <p onClick={() => { 
+            setFeatPupId(puppy.id)}}
+            key={puppy.id}>{puppy.name}</p>;
+          <p key={puppy.id}>{puppy.name}</p>;
+          <p key={puppy.id}>{puppy.name}</p>;
+          <p key={puppy.id}>{puppy.name}</p>;
+          <p key={puppy.id}>{puppy.name}</p>;
+          <p key={puppy.id}>{puppy.name}</p>;
+          <p key={puppy.id}>{puppy.name}</p>;
+
+      </div>
     </div>
   );
 }
